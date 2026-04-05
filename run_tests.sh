4689b61a9fa745532a8359249bb99778db4aad58
Nardy Michelle <102429234+Nardy11@users.noreply.github.com>
2026-04-05 19:53:34 +0200

    Update run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
-pytest tests/
+
+echo "====================================="
+echo "Running all tests using python -m pytest"
+echo "====================================="
+
+python -m pytest
+
+if [ $? -eq 0 ]; then
+    echo "====================================="
+    echo "All tests passed successfully!"
+    echo "====================================="
+else
+    echo "====================================="
+    echo "Some tests failed."
+    echo "====================================="
+fi
